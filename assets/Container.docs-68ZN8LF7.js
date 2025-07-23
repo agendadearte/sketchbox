8f751910efeaa9ca1ead3f69595f981188f327fb
Github Pages Overwriter <GithubPagesOverwriter@users.noreply.github.com>
2025-07-23 09:35:57 +0000

    Automated publish

---
+++ assets/Container.docs-68ZN8LF7.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as n}from"./index-RmkNM0o4.js";import{M as e,T as i,P as m,C as p}from"./blocks-_7QdQ7vi.js";import{P as a}from"./PropertiesList-CmDUJnbN.js";import{C as c}from"./Container.stories-DENNL1pe.js";import{c as x,a as j}from"./Container-Cs22VP3y.js";import"./iframe-HMtenD9h.js";import"./index-B-DXJzQh.js";import"./buildPropName-tGa06a4p.js";import"./buildCustomProps-Dp38_bXo.js";function s(t){const r={h2:"h2",...n(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:c}),`
+`,o.jsx(i,{children:"Container"}),`
+`,o.jsx(m,{}),`
+`,o.jsx(r.h2,{id:"props",children:"Props"}),`
+`,o.jsx(p,{}),`
+`,o.jsx(r.h2,{id:"css-custom-properties",children:"CSS custom properties"}),`
+`,o.jsx(a,{component:j,props:x})]})}function X(t={}){const{wrapper:r}={...n(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(s,{...t})}):s(t)}export{X as default};
